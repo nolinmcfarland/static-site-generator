290aa1452c68735490049b9811b2f960519ed299
Nolin McFarland <nolin.mcfarland@gmail.com>
2025-10-20 14:55:27 -0400

    feat: copy static to public

--- src/main.py
+++ src/main.py
@@ -1,9 +1,40 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
 
 def main():
-    node = TextNode('print("hello world")', TextType.CODE, None)
-    print(node)
+    source = "./static"
+    destination = "./public"
+    if not os.path.exists(source):
+        raise Exception(f"source does not exist at {source}")
+    if os.path.exists(destination):
+        print(f"Removing directory {destination}")
+        remove_path(destination)
+    copy_path(source, destination)
+
+
+def remove_path(path):
+    if os.path.isfile(path):
+        os.remove(path)
+    else:
+        for subpath in os.listdir(path):
+            remove_path(os.path.join(path, subpath))
+        os.rmdir(path)
+
+
+def copy_path(path, destination):
+    if os.path.isfile(path):
+        print(f"Copying file from {path} to {destination}")
+        shutil.copy(path, destination)
+    else:
+        if not os.path.exists(destination):
+            print(f"Making directory {destination}")
+            os.mkdir(destination)
+        for subpath in os.listdir(path):
+            copy_path(
+                os.path.join(path, subpath),
+                os.path.join(destination, subpath)
+            )
 
 
 if __name__ == "__main__":
